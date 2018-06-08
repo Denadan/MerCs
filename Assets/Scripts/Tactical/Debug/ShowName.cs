3d18f75585ed9c2b4c976e91098679fe8d031f67
Denadan <Denadan.sa@gmail.com>
2018-06-08 16:44:13 +0300

    hp4.1 changes in hptable

---
+++ Assets/Scripts/Tactical/Debug/ShowName.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Mercs.Debug
+{
+    public class ShowName : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerExitHandler
+    {
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            UnityEngine.Debug.Log(gameObject.name + " press");
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            UnityEngine.Debug.Log(gameObject.name + " enter");
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            UnityEngine.Debug.Log(gameObject.name + " leave");
+        }
+    }
+}
